6e7c6061c572d024f3521d346764a30051b8c58e
ruok02 <ruok02@naver.com>
2025-12-01 22:26:07 +0900

    복구

--- frontend/public/register_Start.js
+++ frontend/public/register_Start.js
@@ -79,7 +79,7 @@ window.addEventListener('DOMContentLoaded', () => {
             } else {
                 participantCount = null; // 인원수 미정
             }
-            
+                        
             // 임시 데이터 객체 생성
             const tempData = {
                 title: titleVal,
